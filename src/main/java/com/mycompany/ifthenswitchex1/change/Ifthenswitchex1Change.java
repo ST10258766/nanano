f3f0152d3c97ccb8c120681a16e447afedd9bf15
ST10258766 <ST10258766@vcconnect.edu.za>
2023-04-26 08:26:36 +0000

    intial commit

---
+++ src/main/java/com/mycompany/ifthenswitchex1/change/Ifthenswitchex1Change.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ifthenswitchex1.change;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Ifthenswitchex1Change {
+
+    public static void main(String[] args) {
+        int day = 4;
+        
+        switch(day){
+            case 0: System.out.println("Sunday");
+    break;
+            case 1:
+        System.out.println("Monday");
+     break;
+            case 2:
+        System.out.println("tuesday");
+        break;
+            case 3:
+        System.out.println("Wedesday");
+         break;
+            case 4:
+        System.out.println("Thursday");
+        break;
+            case 5:
+        System.out.println("Friday");
+        break;
+            case 6:
+        System.out.println("Saturday");
+        break;
+            default:
+        System.out.println("NOT A VALID RANGE");
+              
+        
+}
+    }
+    }
+
